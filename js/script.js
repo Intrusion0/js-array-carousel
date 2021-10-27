719d8a3f220d2323f19a28eed4f4059615b71ad9
Mario <lombardomario01@gmail.com>
2021-10-27 18:31:29 +0200

    Ex Completed

--- js/script.js
+++ js/script.js
@@ -3,7 +3,6 @@ Dati tre array contenenti: - una lista ordinata di 5 immagini, - una lista ordin
 Al click dell’utente sulle frecce verso l’alto o verso il basso, l’immagine attiva diventa visibile in formato grande a sinistra e nel suo angolo in basso a destra dovranno essere aggiunti i relativi - titolo e - testo.
 Allo stesso tempo nelle miniature l’immagine attiva dovrà apparire in evidenza rispetto alle altre.
 */
-console.log("Ciaooo eccomi!!");
 
 const items = [
     'img/01.jpg',
@@ -28,3 +27,67 @@ const text = [
     'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
     'Et temporibus voluptatum suscipit tempore aliquid deleniti aut veniam inventore eligendi ex ad ullam,',
 ]
+
+const pictCont = document.querySelector(".main_picture");
+const thumbsCont = document.querySelector(".thumbs");
+
+let picture = "";
+let thumbs = "";
+
+for (let i = 0; i < items.length; i++) {
+    picture += `
+    <div class="picture">
+        <img src="${items[i]}" alt="${title[i]}">
+        <div class="text">
+            <h3>${title[i]}</h3>
+            <p>${text[i]}</p>
+        </div>
+    </div>`
+
+    thumbs += `
+    <div class="thumb">
+        <img src="${items[i]}" alt="${title[i]}">
+    </div>`
+}
+
+// Output con active in posizione di default
+pictCont.innerHTML = picture;
+document.getElementsByClassName("picture") [0].classList.add("active");
+
+thumbsCont.innerHTML += thumbs;
+document.getElementsByClassName("thumb") [0].classList.add("active");
+
+// Evento click sul pulsante "next"
+let nextThumb = document.querySelector(".next");
+
+// Creo una variabile di conteggio
+let contPos = 0;
+
+nextThumb.addEventListener('click',
+    function() {
+
+        ++contPos
+
+        document.querySelector(".picture.active").classList.remove("active");
+        document.getElementsByClassName("picture") [contPos].classList.add("active");
+
+        document.querySelector(".thumb.active").classList.remove("active");
+        document.getElementsByClassName("thumb") [contPos].classList.add("active");
+    }
+)
+
+// Evento click sul pulsante "prev"
+let prevThumb = document.querySelector(".prev");
+
+prevThumb.addEventListener('click',
+    function() {
+
+        --contPos
+
+        document.querySelector(".picture.active").classList.remove("active");
+        document.getElementsByClassName("picture") [contPos].classList.add("active");
+
+        document.querySelector(".thumb.active").classList.remove("active");
+        document.getElementsByClassName("thumb") [contPos].classList.add("active");
+    }
+)
